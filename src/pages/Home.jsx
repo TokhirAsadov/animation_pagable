11a11d9215fba952fb831e15f630b808a2fb65f7
TokhirAsadov <86370041+TokhirAsadov@users.noreply.github.com>
2022-03-20 16:43:14 +0500

    created Home.jsx . It is necessary for me to work with home of my site.

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Popular from "../component/Popular";
+import Veggie from "../component/Veggie";
+
+const Home = () => {
+    return (
+        <div>
+            <Popular />
+            <Veggie />
+        </div>
+    );
+};
+
+export default Home;
