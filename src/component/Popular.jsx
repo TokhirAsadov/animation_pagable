6aade1c76e928e8a9b77b702f5e9e8acc448f93b
TokhirAsadov <86370041+TokhirAsadov@users.noreply.github.com>
2022-03-20 21:08:48 +0500

    full done Popular.jsx

--- src/component/Popular.jsx
+++ src/component/Popular.jsx
@@ -1,11 +1,109 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import styled from 'styled-components';
+import {Splide,SplideSlide} from '@splidejs/react-splide';
+import '@splidejs/splide/dist/css/splide.min.css';
 
 const Popular = () => {
+
+    const [popular,setPopular] = useState([]);
+
+    useEffect(() => {
+        getPopular();
+    },[])
+
+    const getPopular = async () => {
+        const check = localStorage.getItem("popular");
+
+        if(check){
+            setPopular(JSON.parse(check));
+        }else {
+            const api = await
+                fetch(
+                    `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+                );
+            const data = await api.json();
+            localStorage.setItem("popular",JSON.stringify(data.recipes));
+            setPopular(data.recipes);
+        }
+
+    }
+    
     return (
         <div>
-            Popular
+
+            <Wrapper>
+                <h3>Popular Picks</h3>
+
+                <Splide options={{
+                    perPage:4,
+                    arrows:false,
+                    pagination:false,
+                    drag:'free',
+                    gap:'2rem'
+                }}>
+                    {
+                        popular.map( recipe => {
+                           return(
+                               <SplideSlide key={recipe.id}>
+                                  <Card>
+                                      <Title>{recipe.title}</Title>
+                                      <Img className="splideSlide" src={recipe.image} alt={recipe.title}/>
+                                      <Gradient />
+                                  </Card>
+                               </SplideSlide>
+                           ) ;
+                        })
+                    }
+                </Splide>
+            </Wrapper>
+
         </div>
     );
 };
+
+const Wrapper = styled.div`
+    margin:4rem 0rem!important;
+`;
+
+const Card = styled.div`
+    min-height:12rem;
+    overflow:hidden;
+    border-radius:2rem;
+    margin:2rem 0rem 0.2rem;
+    display: flex;
+    flex-direction: column;
+    position:relative;
+`;
+
+const Title = styled.p`
+    width:100%;
+    height:40%;
+    font-weight:600;
+    font-size:1rem;
+    text-align:center;
+    position:absolute;
+    left:50%;
+    bottom:0%;
+    color:white;
+    z-index:100;
+    transform:translate(-50%,0%);
+`
+
+const Img = styled.img`
+    width:100%;
+    height:100%;
+    position:absolute;
+    left:0;
+    object-fit: cover;
+    display:block;
+`;
+
+const Gradient = styled.div`
+    width:100%;
+    height:100%;
+    z-index:3;
+    position:absolute;
+    background: linear-gradient(rgba(0,0,0,0),rgba(0,0,0,0.5));
+`;
 
 export default Popular;
