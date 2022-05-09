de6d0d0bdcfc72c3cfa85c5d25b20bd54b797b7d
Tohir <guvalakat1603@gmail.com>
2022-05-09 19:39:53 +0500

    added BrowserRouter as Router from react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Pages from "./pages/Pages";
+import Category from "./component/Category";
+import {BrowserRouter as Router} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h2 style={{fontWeight:400}}>Hello my project</h2>
+    <Router>
+        <Category />
         <Pages />
-    </div>
+    </Router>
   );
 }
 
